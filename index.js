e5c8e87652f8f2952439cb6a937ed4941a3700bf
xedter20 <dextermiranda441@gmail.com>
2024-02-01 00:38:59 +0800

    intial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const player = require('play-sound')((opts = {}));
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/playAudioAlarm', (req, res) => {
+  player.play('audio/emergency-alarm.mp3', function (err) {
+    if (err) throw err;
+    res.json({
+      success: true
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
